8a9411f2b38c003e63f22d3c924d58e4e28c02ff
Guy Ludvig <guy@frontegg.com>
2026-02-25 16:00:19 +0200

    First test

--- tests/playwright.config.ts
+++ tests/playwright.config.ts
@@ -1,2 +1,33 @@
-export default playwrightConfig;
+import {PlaywrightTestConfig} from '@playwright/test';
 
+import {TestDirectory} from './utils/test-directory';
+import {getReporters} from './getReporters';
+
+const playwrightConfig: PlaywrightTestConfig = {
+    use: {
+        headless: false,
+        ignoreHTTPSErrors: true,
+        testIdAttribute: '',
+        video: 'retain-on-failure',
+        screenshot: 'only-on-failure',
+        trace: 'retain-on-failure',
+        baseURL: 'http://localhost:3000',
+    },
+    projects: [
+        {
+            name: 'tests',
+            testDir: TestDirectory.ALL,
+            testIgnore: [],
+            testMatch: /.*.spec.ts/
+        }
+    ],
+    outputDir: 'test-results/',
+    reporter: getReporters(),
+    testMatch: /.*.spec.ts/,
+    timeout: 120 * 1000,
+    expect: { timeout: 60 * 1000 },
+    retries: 1,
+    workers: 1
+}
+
+export default playwrightConfig;
